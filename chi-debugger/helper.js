22837ccb2a10af0ad6291921b481335c17e76a59
chillinhen <c.hillen@gmx.net>
2018-03-11 09:18:09 +0100

    added helper.js & other

---
+++ chi-debugger/helper.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+//detect unintended horizontal scrolling
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+});
